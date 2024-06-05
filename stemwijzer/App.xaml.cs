a752d4dcf5b0ee34e36087364864b715a93ed22c
Abdu <ps251232@summacollege.nl>
2024-06-05 13:29:08 +0200

    Add project files.

---
+++ stemwijzer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace stemwijzer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
